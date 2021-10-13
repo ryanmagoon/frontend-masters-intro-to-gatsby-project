894264bb077417352bae970d13de0c917b7c4523
Ryan Magoon <mr.magoon5@gmail.com>
2021-10-12 22:56:41 -0400

    progress

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
+import { Link } from 'gatsby';
 import * as React from 'react';
 
 export default function Index() {
   return (
     <main>
       <h1>Hello Frontend Masters!</h1>
+      <Link to="/about">About</Link>
     </main>
   );
 }
